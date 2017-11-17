eec04720f337232b947a1f519707c90d63a563f4
Shlygly <Anthony.Champi.Dupont@gmail.com>
2017-11-17 10:40:37 +0100

    Add some features and fix some stuff

--- extBridge.py
+++ extBridge.py
@@ -2,7 +2,7 @@ import hexchat
 import re
 
 __module_name__ = 'External Bridge Adaptor'
-__module_version__ = '1.0'
+__module_version__ = '1.1'
 __module_description__ = 'Adapt Hexchat UI for external bridges'
 __author__ = 'Stockage'
 
@@ -19,11 +19,17 @@ re_rename_format = "^(.+) is now known as (.+)$"
 cmd_nick = None
 
 def EmitMsg(nick, message, mode):
-    highlight = hexchat.get_info("nick") in message
-    same_user = hexchat.get_info("nick") == nick
-    hexchat.emit_print("Channel Msg Hilight" if highlight else "Channel Message", ("\x0304" if same_user else "") + nick, message, mode, "\00306" + nick_prefix + ("\00303" if highlight else "\00302"))
+    same_user = hexchat.nickcmp(hexchat.get_info("nick").lower(), nick.lower()) == 0
+    hilight = not same_user and hexchat.get_info("nick").lower() in message.lower()
+    if (same_user):
+        hexchat.emit_print("Your Message", nick, message, mode, "\00306" + nick_prefix + "\00304")
+    elif (hilight):
+        hexchat.emit_print("Channel Msg Hilight", nick, message, mode, "\00306" + nick_prefix + "\00303")
+    else:
+        hexchat.emit_print("Channel Message", nick, message, mode, "\00306" + nick_prefix + "\00302")
+    # hexchat.emit_print("Channel Msg Hilight" if hilight else "Channel Message", ("\x0304" if same_user else "") + nick, message, mode, "\00306" + nick_prefix + ("\00303" if hilight else "\00302"))
     # TODO : Gérer la couleur actuelle du chan
-    hexchat.command("GUI COLOR {}".format("3" if highlight else "2"))
+    hexchat.command("GUI COLOR {}".format("3" if hilight else "2"))
 
 def msg_cmd(word, word_eol, userdata):
     global cmd_nick
@@ -31,11 +37,12 @@ def msg_cmd(word, word_eol, userdata):
         # Classic message
         if (len(re.findall(re_msg_format, word[1])) > 0):
             nick, message = re.findall(re_msg_format, word[1])[0]
+            nick = hexchat.strip(nick)
             EmitMsg(nick, message, word[2])
             return hexchat.EAT_HEXCHAT
         # Command (part. 1)
         elif (len(re.findall(re_cmd_format, word[1])) > 0):
-            cmd_nick = re.findall(re_cmd_format, word[1])[0]
+            cmd_nick = hexchat.strip(re.findall(re_cmd_format, word[1])[0])
             return hexchat.EAT_HEXCHAT
         # Command (part. 2)
         elif (cmd_nick != None and word[1][0] == "!"):
@@ -44,17 +51,19 @@ def msg_cmd(word, word_eol, userdata):
             return hexchat.EAT_HEXCHAT
         # Someone joined
         elif (len(re.findall(re_join_format, word[1])) > 0):
-            nick = re.findall(re_join_format, word[1])[0]
+            nick = hexchat.strip(re.findall(re_join_format, word[1])[0])
             hexchat.command("RECV :{}!~{}@{} JOIN {}".format(nick, bot_nick, bot_vhost, hexchat.get_info("channel")))
             return hexchat.EAT_HEXCHAT
         # Someone quit
         elif (len(re.findall(re_quit_format, word[1])) > 0):
-            nick = re.findall(re_quit_format, word[1])[0]
+            nick = hexchat.strip(re.findall(re_quit_format, word[1])[0])
             hexchat.command("RECV :{}!~{}@{} QUIT {}".format(nick, bot_nick, bot_vhost, quit_message))
             return hexchat.EAT_HEXCHAT
         # Someone change his nick
         elif (len(re.findall(re_rename_format, word[1])) > 0):
             old_nick, new_nick = re.findall(re_rename_format, word[1])[0]
+            old_nick = hexchat.strip(old_nick)
+            new_nick = hexchat.strip(new_nick)
             hexchat.command("RECV :{}!~{}@{} NICK {}".format(old_nick, bot_nick, bot_vhost, new_nick))
             return hexchat.EAT_HEXCHAT
     return hexchat.EAT_NONE
@@ -70,3 +79,5 @@ hooks = [
 ]
 
 hexchat.hook_unload(unload)
+
+print("\00307{} v{}\017 : Connected width \00306Discord !".format(__module_name__, __module_version__))
